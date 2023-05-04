3b1c0a7539c21e4b199cdfb5cd9c8418c0deec23
Frempong <samankwaa.frempong@gmail.com>
2023-05-04 23:20:22 +0000

    Add factorial test case(3)

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe "Solver" do
             expect(solver.factorial(0)).to eq(1)
             expect(solver.factorial(1)).to eq(1)
             expect(solver.factorial(2)).to eq(2)
+            expect(solver.factorial(3)).to eq(6)
         end
     end
 end
