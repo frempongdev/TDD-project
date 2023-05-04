a2292a1d1bef6efc0c3e91e9f63e709ffab93fbf
Frempong <samankwaa.frempong@gmail.com>
2023-05-04 23:11:44 +0000

    Add factorial test case(1)

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -4,8 +4,9 @@ RSpec.describe "Solver" do
     context "factorial" do
         it "multiplies of all integers from 1 to a number" do
             solver = Solver.new
-            
+
             expect(solver.factorial(0)).to eq(1)
+            expect(solver.factorial(1)).to eq(1)
         end
     end
 end
