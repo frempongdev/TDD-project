290826ae0d7e59fc59f0e5b40402b13c3a537e8f
Frempong <samankwaa.frempong@gmail.com>
2023-05-04 23:06:59 +0000

    Refactor test factorial method with instance of Solver class

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -3,7 +3,9 @@ require './solver.rb'
 RSpec.describe "Solver" do
     context "factorial" do
         it "multiplies of all integers from 1 to a number" do
-            expect(factorial(0)).to eq(1)
+            solver = Solver.new
+            
+            expect(solver.factorial(0)).to eq(1)
         end
     end
 end
