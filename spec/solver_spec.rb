ff55e8456c1ec60fc33040302577358301fbb11c
jonas-45 <jonaswullah@gmail.com>
2023-05-05 09:51:48 +0000

    Apply DRY principle to creating of solver class instance in testing

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,30 +1,32 @@
 require './solver.rb'
 
 RSpec.describe "Solver" do
+  before(:all) do
+    @solver = Solver.new
+   end
+
     context "factorial" do
         it "multiplies of all integers from 1 to a number" do
-            solver = Solver.new
 
-            expect(solver.factorial(0)).to eq(1)
-            expect(solver.factorial(1)).to eq(1)
-            expect(solver.factorial(2)).to eq(2)
-            expect(solver.factorial(3)).to eq(6)
-            expect(solver.factorial(5)).to eq(120)
-            expect{solver.factorial(-5)}.to raise_error( ArgumentError, "Number must be non-negative")
+            expect(@solver.factorial(0)).to eq(1)
+            expect(@solver.factorial(1)).to eq(1)
+            expect(@solver.factorial(2)).to eq(2)
+            expect(@solver.factorial(3)).to eq(6)
+            expect(@solver.factorial(5)).to eq(120)
+            expect{@solver.factorial(-5)}.to raise_error( ArgumentError, "Number must be non-negative")
         end
 
         it "reverses a string" do
-            solver = Solver.new
 
-            expect(solver.reverse('eye')).to eq('eye')
-            expect(solver.reverse('hello')).to eq('olleh')
-            expect(solver.reverse('microverse')).to eq('esrevorcim')
+            expect(@solver.reverse('eye')).to eq('eye')
+            expect(@solver.reverse('hello')).to eq('olleh')
+            expect(@solver.reverse('microverse')).to eq('esrevorcim')
         end
     end
 
     context 'fizzbuzz' do
       it 'return fizz when number is divisble by 3' do
-        expect(fizzbuzz(3)).to eq 'fizz'
+        expect(@solver.fizzbuzz(3)).to eq 'fizz'
       end
     end
 end
