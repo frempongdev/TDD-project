0caec4907e71fd9e7af75548c3d95287baf7282b
jonas-45 <jonaswullah@gmail.com>
2023-05-05 11:01:41 +0000

    Resolve linter errors

--- solver.rb
+++ solver.rb
@@ -1,27 +1,27 @@
 class Solver
-    def factorial(number)
-        if number < 0
-            raise ArgumentError, "Number must be non-negative"
-        elsif number == 0 || number == 1
-            1
-         else
-            number * factorial(number - 1)
-         end
+  def factorial(number)
+    if number.negative?
+      raise ArgumentError, 'Number must be non-negative'
+    elsif [0, 1].include?(number)
+      1
+    else
+      number * factorial(number - 1)
     end
+  end
 
-    def reverse(word)
-        word.reverse
-    end
+  def reverse(word)
+    word.reverse
+  end
 
-    def fizzbuzz(num)
-      if num%3 == 0 && num%5 == 0 
-        'fizzbuzz'
-      elsif num%5 == 0
-        'buzz'
-      elsif num%3 == 0
-        'fizz'
-      else
-        "#{num}"
-      end
+  def fizzbuzz(num)
+    if (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 5).zero?
+      'buzz'
+    elsif (num % 3).zero?
+      'fizz'
+    else
+      num.to_s
     end
+  end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,48 +1,46 @@
-require './solver.rb'
+require './solver'
 
-RSpec.describe "Solver" do
+RSpec.describe 'Solver' do
   before(:all) do
     @solver = Solver.new
-   end
+  end
 
-    context "factorial" do
-        it "multiplies of all integers from 1 to a number" do
-
-            expect(@solver.factorial(0)).to eq(1)
-            expect(@solver.factorial(1)).to eq(1)
-            expect(@solver.factorial(2)).to eq(2)
-            expect(@solver.factorial(3)).to eq(6)
-            expect(@solver.factorial(5)).to eq(120)
-            expect{@solver.factorial(-5)}.to raise_error( ArgumentError, "Number must be non-negative")
-        end
-
-        it "reverses a string" do
-
-            expect(@solver.reverse('eye')).to eq('eye')
-            expect(@solver.reverse('hello')).to eq('olleh')
-            expect(@solver.reverse('microverse')).to eq('esrevorcim')
-        end
+  context 'factorial' do
+    it 'multiplies of all integers from 1 to a number' do
+      expect(@solver.factorial(0)).to eq(1)
+      expect(@solver.factorial(1)).to eq(1)
+      expect(@solver.factorial(2)).to eq(2)
+      expect(@solver.factorial(3)).to eq(6)
+      expect(@solver.factorial(5)).to eq(120)
+      expect { @solver.factorial(-5) }.to raise_error(ArgumentError, 'Number must be non-negative')
     end
 
-    context 'fizzbuzz' do
-      it 'return fizz when number is divisble by 3' do
-        expect(@solver.fizzbuzz(3)).to eq 'fizz'
-        expect(@solver.fizzbuzz(21)).to eq 'fizz'
-      end
-
-      it 'return buzz when number is divisble by 5' do
-        expect(@solver.fizzbuzz(10)).to eq 'buzz'
-        expect(@solver.fizzbuzz(20)).to eq 'buzz'
-      end
-
-      it 'return fizzbuzz when number is divisble by 3 and 5' do
-        expect(@solver.fizzbuzz(15)).to eq 'fizzbuzz'
-        expect(@solver.fizzbuzz(30)).to eq 'fizzbuzz'
-      end
-
-      it 'return number if not divisible by 3 or 5' do
-        expect(@solver.fizzbuzz(2)).to eq '2'
-        expect(@solver.fizzbuzz(4)).to eq '4'
-      end
+    it 'reverses a string' do
+      expect(@solver.reverse('eye')).to eq('eye')
+      expect(@solver.reverse('hello')).to eq('olleh')
+      expect(@solver.reverse('microverse')).to eq('esrevorcim')
     end
+  end
+
+  context 'fizzbuzz' do
+    it 'return fizz when number is divisble by 3' do
+      expect(@solver.fizzbuzz(3)).to eq 'fizz'
+      expect(@solver.fizzbuzz(21)).to eq 'fizz'
+    end
+
+    it 'return buzz when number is divisble by 5' do
+      expect(@solver.fizzbuzz(10)).to eq 'buzz'
+      expect(@solver.fizzbuzz(20)).to eq 'buzz'
+    end
+
+    it 'return fizzbuzz when number is divisble by 3 and 5' do
+      expect(@solver.fizzbuzz(15)).to eq 'fizzbuzz'
+      expect(@solver.fizzbuzz(30)).to eq 'fizzbuzz'
+    end
+
+    it 'return number if not divisible by 3 or 5' do
+      expect(@solver.fizzbuzz(2)).to eq '2'
+      expect(@solver.fizzbuzz(4)).to eq '4'
+    end
+  end
 end
